0f9ba1846cbc1df6c26f2263536e6a1ab5030526
ryamaoka-cq <ryota.yamaoka7321@gmail.com>
2022-10-06 18:26:55 +0900

    bin/cake bake all users & hash password

---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'email' => 'Lorem ipsum dolor sit amet',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'created' => '2022-10-06 09:24:07',
+                'modified' => '2022-10-06 09:24:07',
+            ],
+        ];
+        parent::init();
+    }
+}
